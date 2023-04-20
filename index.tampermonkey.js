b50d72d38c7259846e81b0ccb133fca4144771fc
shrimpbighead <857763541@qq.com>
2023-04-20 22:43:40 +0800

    Update index.tampermonkey.js
    增加显示是否已经沟通过的状态
    增加了一个过滤（隐藏）相应状态数据的多选框功能

--- index.tampermonkey.js
+++ index.tampermonkey.js
@@ -1,19 +1,37 @@
 // ==UserScript==
 // @name         showBossActiveTime
 // @namespace    http://www.chensong.cc/
-// @version      0.1
+// @version      0.3
 // @description  to show hr lastest login time,help you deliver your resume efficiently.
 // @author       chensong
 // @match        https://www.zhipin.com/web/geek/job*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant       GM_xmlhttpRequest
+// @license     MIT 
 // ==/UserScript==
 
 (function () {
   'use strict';
 
   function showBossActiveTime() {
-    let isPageChange = false;
+    let isUpdating = false;
+    let statusOptions = localStorage.getItem('bossActiveStatusList')?.split(',')||['半年前活跃', '近半年活跃','4月前活跃','2月内活跃','2周内活跃'];
+    let removeStatusList = [];
+
+    const style = `
+    #removeFilterDataContainer{
+      position: fixed;right: 70px;top: 70px;z-index: 20000;background: #00bebd; color: #fff;display: flex;flex-direction: column;padding-bottom:10px
+    }
+    #removeFilterDataContainer.hide{height:28px;overflow:hidden}
+    #removeFilterDataContainer.hide #boss-active-time-arrow svg{transform: rotate(180deg);}
+    #removeFilterDataContainer #boss-active-time-arrow {cursor: pointer; display: flex;justify-content: flex-end;font-size: 24px;background: #009796;padding:2px 10px;}
+    #removeFilterDataContainer .tips{font-size:16px;margin:5px 20px;}
+    #removeFilterDataContainer label{display:flex;padding:0 20px;}
+    #removeFilterDataContainer label input{margin-right:5px;}
+    `;
+    const styleEle = document.createElement('style');
+    styleEle.innerHTML = style;
+    document.head?.appendChild(styleEle);
 
     function getLis() {
       /*要把在线的过滤掉，一来减少请求，二来请求的数据也不存在下面代码的class .boss-active-time，会报错'*/
@@ -29,23 +47,37 @@
       return links;
     }
 
-    function setText(node, statusTxt) {
+    function setText(node, statusText) {
       let pNode = document.createElement('p');
-      pNode.innerHTML = statusTxt;
-      console.log(statusTxt);
+      pNode.className = 'status';
       pNode.style.cssText =
         'display:flex;padding:5px 10px;background:#e1f5e3;color:green;width:80%;border-radius:4px;margin-top:10px;';
       node.querySelector('.job-info').after(pNode);
       pNode.parentNode.style.height = 'auto';
       pNode.parentNode.style.paddingBottom = '0';
+      let chatBtn = node.querySelector('.start-chat-btn');
+      let chatState = chatBtn.textContent==='立即沟通'?'':'\n&nbsp;&nbsp;&nbsp;&nbsp;已经沟通过了';
+      pNode.innerHTML = statusText+chatState;
+      pNode.setAttribute('status',statusText);
+      //console.log(statusText);
+      // 隐藏要过滤的数据
+      if(removeStatusList.length){
+        setDomDisplay(node,statusText);
+      }
+      // 保存一下状态数据
+      if(!statusOptions.includes(statusText)&&statusText!=='在线'){
+        statusOptions.push(statusText);
+        localStorage.setItem('bossActiveStatusList',statusOptions);
+      }
     }
 
     async function getListStatus() {
       alertBox('开始更新状态....,网站安全策略问题，更新会比较缓慢。');
       let links = getLis();
+      isUpdating = true;
       for (let i = 0; i < links.length; i++) {
-        if (isPageChange) {
-          /*切换了分页， 要中断循环*/
+        if (!isUpdating) {
+        //  有方法通知更新需要中断，停止循环
           break;
         }
         let node = links[i];
@@ -58,7 +90,7 @@
               url: link,
               onload: function (response) {
                 if (/security-check.html/.test(response.finalUrl)) {
-                  console.log(response.finalUrl, 888);
+                 // console.log(response.finalUrl, 888);
                   //    用GM_xmlhttpRequest获取触发了302，用finaleUrl通过iframe来获取，不用link,看是否能省略302这个步骤,加快速度
                   getStatusByIframe(response.finalUrl, i).then((res) => {
                     if (res === '') {
@@ -79,7 +111,9 @@
           }, 1000);
         });
         if (i === links.length - 1) {
+          isUpdating = false;
           alertBox('更新完成');
+          
         }
       }
     }
@@ -114,9 +148,9 @@
       /*因为分页每次点击都会重新渲染，所以需要点击后用定时器重新运行方法"*/
       for (let i = 0; i < pages.length; i++) {
         pages[i].addEventListener('click', function () {
-          isPageChange = true;
+          isUpdating = false;
           setTimeout(function () {
-            showBossActiveTime();
+            init();
           }, 1000);
         });
       }
@@ -150,12 +184,86 @@
         return status;
       }
     }
+    function addStatusFilter(){
+      const container = document.createElement('div');
+      container.id='removeFilterDataContainer';
+      const tips = document.createElement('div');
+      tips.innerHTML = '过滤掉勾选的数据';
+      tips.className = 'tips';
+      const arrow = document.createElement('div');
+      arrow.id='boss-active-time-arrow';
+      arrow.innerHTML = '<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7"></path></svg>';
+      container.appendChild(arrow);
+      container.appendChild(tips);
+      arrow.addEventListener('click',function(){
+        container.classList.contains('hide')?container.classList.remove('hide'):container.classList.add('hide');
+      })
+
+      statusOptions.forEach(option => {
+        const label = document.createElement('label');
+        const el = document.createElement('input');
+        el.type = 'checkbox';
+        el.name = option;
+        el.value = option;
+        label.appendChild(el);
+        label.appendChild(document.createTextNode(option));
+        container.appendChild(label);
+      });
+
+
+      const loopDom = function(){
+        const nodes = Array.from(
+          document.querySelectorAll('.job-list-box .job-card-left')
+        ).filter((node) => {
+          let online = node.querySelector('.boss-online-tag');
+          return !online;
+        });
+        for (const node of nodes) {
+          const pNode = node.querySelector('.status');
+          // 有些数据还没获取到状态
+          if(pNode){
+            const status = pNode.getAttribute('status');
+            setDomDisplay(node,status);
+          }
+        }
+      }
+
+      container.addEventListener('change', function(event) {
+        const selectedValues = Array.from(container.querySelectorAll('input[type="checkbox"]:checked')).map(el => el.value);
+        removeStatusList = selectedValues;
+        loopDom();
+        // 如果正在更新,需要定时器定时查询最新数据，不然后面更新的数据不能触发loopDom
+        if(isUpdating){
+          const timer = setInterval(()=>{
+            // 如果已经更新完毕，清除定时器,这里面多次选择会触发多个定时器，可以把timer提到外面避免这个问题，但因为实际使用中，重复调用的代价并不明显，也因为懒，就不提出到外面了
+            if(!isUpdating){
+              clearInterval(timer);
+            }
+            loopDom();
+          },2000);
+        }
+      });
+
+      document.body.appendChild(container);
+
+    }
+    function setDomDisplay(node,statusText){
+      const liNode = node.parentElement.parentElement;
+      if(removeStatusList.includes(statusText)){
+        liNode.style.display = 'none';
+      }else{
+        liNode.style.display = 'block';
+      }
+    }
+    
+
     function init() {
       pageBtnBindClick();
       getListStatus();
     }
-
+    
     init();
+    addStatusFilter();
   }
   window.onload = function () {
     setTimeout(() => {
